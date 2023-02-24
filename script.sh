6bbc6f21688db2c7745d337ad64c43f3a53159e3
mortaty <126246192+mortaty@users.noreply.github.com>
2023-02-24 15:58:24 +0000

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+echo "I am being run from a script"
+touch file3.txt file4.txt file5.txt
+echo "Created file3.txt file4.txt and file5.txt"
+echo "Listing current directory contents"
+ls-al
